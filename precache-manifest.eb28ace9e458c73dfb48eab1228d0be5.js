f438c023c867424bc35d396c6d10bf497e8a940d
ravi yadav <raviyadav00063@gmail.com>
2020-04-15 23:46:40 +0530

    Updates

---
+++ precache-manifest.eb28ace9e458c73dfb48eab1228d0be5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7c50f232dd55012d67fe450e8fb30f",
+    "url": "/COVID-19/index.html"
+  },
+  {
+    "revision": "569496d3734e7a3b1912",
+    "url": "/COVID-19/static/css/main.8aaa07e3.chunk.css"
+  },
+  {
+    "revision": "97563700f73154d40ea0",
+    "url": "/COVID-19/static/js/2.47347ee0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19/static/js/2.47347ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "569496d3734e7a3b1912",
+    "url": "/COVID-19/static/js/main.39039d96.chunk.js"
+  },
+  {
+    "revision": "5bb6ac3d72cea80b063c",
+    "url": "/COVID-19/static/js/runtime-main.edae6198.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19/static/media/image.d7265326.png"
+  }
+]);
